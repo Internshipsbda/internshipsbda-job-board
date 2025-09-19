710e229e0b68c78ac5688d983196c566ef8862aa
Internshipsbda <sydney.west@internshipinsightsco.com>
2025-09-19 13:18:04 -0300

    Update and rename .gitkeep to slug.js

---
+++ scripts/slug.js
@@ -0,0 +1,12 @@
+function slugify(str) {
+  return str
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+}
+
+if (typeof module !== "undefined") {
+  module.exports = { slugify };
+}
+
